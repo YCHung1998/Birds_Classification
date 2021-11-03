eddb8af17368638156fbf7138ab34331a4445260
YCHung1998 <93210989+YCHung1998@users.noreply.github.com>
2021-11-03 14:57:33 +0800

    Update Prepare_data.py

--- src/Prepare_data.py
+++ src/Prepare_data.py
@@ -4,11 +4,26 @@ from src.Transform import Transforms
 from src.Dataset import VRDL_BirdDataset
 
 T = Transforms()
-train_transforms, valid_transforms = T.train_transforms(), T.valid_transforms()
+train_transforms = T.train_transforms()
+valid_transforms = T.valid_transforms()
 data_root = r'/data/S/LinGroup/Users/YC/VRDL_HW1'
+
+
 def Prepare_data(MBS, fold_list, train_frame, valid_frame):
-    train_set = VRDL_BirdDataset(data_root, fold_list['train'], train_frame, train_transforms)
-    valid_set = VRDL_BirdDataset(data_root, fold_list['valid'], valid_frame, valid_transforms)
-    train_loader = DataLoader(train_set, batch_size=MBS, num_workers=4, shuffle=True)
-    valid_loader = DataLoader(valid_set, batch_size=MBS, num_workers=4, shuffle=False)
+    train_set = VRDL_BirdDataset(data_root,
+                                 fold_list['train'],
+                                 train_frame,
+                                 train_transforms)
+    valid_set = VRDL_BirdDataset(data_root,
+                                 fold_list['valid'],
+                                 valid_frame,
+                                 valid_transforms)
+    train_loader = DataLoader(train_set,
+                              batch_size=MBS,
+                              num_workers=4,
+                              shuffle=True)
+    valid_loader = DataLoader(valid_set,
+                              batch_size=MBS,
+                              num_workers=4,
+                              shuffle=False)
     return train_set, valid_set, train_loader, valid_loader
