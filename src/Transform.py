e714527e71cc8dfcc10428e7fd7e3e63a66cfe7e
YCHung1998 <93210989+YCHung1998@users.noreply.github.com>
2021-11-03 15:33:30 +0800

    Update Transform.py

--- src/Transform.py
+++ src/Transform.py
@@ -1,13 +1,23 @@
-from torchvision import transforms
-from torchvision.io import read_image
 import torch
 import random
+from torchvision.io import read_image
+from torchvision.transforms import (Resize,
+                                    RandomOrder
+                                    RandomHorizontalFlip,
+                                    RandomRotation,
+                                    RandomPerspective,
+                                    RandomResizedCrop,
+                                    RandomAffine,
+                                    RandomCrop,
+                                    Normalize,
+                                    Compose)
 '''
-Simulate monai's Dictionary Transforms writing 
+Simulate monai's Dictionary Transforms writing
 https://docs.monai.io/en/latest/transforms.html#dictionary-transforms
 '''
 
-class read_imaged(): 
+
+class read_imaged():
     def __init__(self, keys):
         self.keys = keys
 
@@ -16,25 +26,26 @@ class read_imaged():
             if key in data:
                 data[key] = read_image(data[key])
             else:
-                 raise KeyError(f'{key} is not a key of {data}')
+                raise KeyError(f'{key} is not a key of {data}')
 
         return data
 
-    
-class Resized(): 
-    def __init__(self, keys, size=(384,384)):
+
+class Resized():
+    def __init__(self, keys, size=(384, 384)):
         self.keys = keys
         self.size = size
+
     def __call__(self, data):
         for key in self.keys:
             if key in data:
-                data[key] = transforms.Resize(self.size)(data[key])
+                data[key] = Resize(self.size)(data[key])
             else:
                 raise KeyError(f'{key} is not a key of {data}')
 
         return data
-    
-    
+
+
 class RandHoriFlipd():
     def __init__(self, keys):
         self.keys = keys
@@ -42,13 +53,13 @@ class RandHoriFlipd():
     def __call__(self, data):
         for key in self.keys:
             if key in data:
-                data[key] = transforms.RandomHorizontalFlip()(data[key])
+                data[key] = RandomHorizontalFlip()(data[key])
             else:
                 raise KeyError(f'{key} is not a key of {data}')
 
         return data
-    
-    
+
+
 class RandRotd():
     def __init__(self, keys, degrees=10):
         self.keys = keys
@@ -57,7 +68,7 @@ class RandRotd():
     def __call__(self, data):
         for key in self.keys:
             if key in data:
-                data[key] = transforms.RandomRotation(self.degrees)(data[key])
+                data[key] = RandomRotation(self.degrees)(data[key])
             else:
                 raise KeyError(f'{key} is not a key of {data}')
 
@@ -71,12 +82,13 @@ class RandPerspectived():  # Not be used
     def __call__(self, data):
         for key in self.keys:
             if key in data:
-                data[key] = transforms.RandomPerspective(distortion_scale=0.6, p=1.0)(data[key])
+                data[key] = RandomPerspective(distortion_scale=0.6,
+                                              p=1.0)(data[key])
             else:
-                raise KeyError(f'{key} is not a key of {data}')        
+                raise KeyError(f'{key} is not a key of {data}')
 
         return data
-        
+
 
 class RandResizedCropd():  # Not be used
     def __init__(self, keys):
@@ -85,9 +97,10 @@ class RandResizedCropd():  # Not be used
     def __call__(self, data):
         for key in self.keys:
             if key in data:
-                data[key] = transforms.RandomResizedCrop(size=300, scale=(0.08, 1))(data[key])
+                data[key] = RandomResizedCrop(size=300,
+                                              scale=(0.08, 1))(data[key])
             else:
-                raise KeyError(f'{key} is not a key of {data}')       
+                raise KeyError(f'{key} is not a key of {data}')
 
         return data
 
@@ -99,13 +112,14 @@ class RandAffined():
     def __call__(self, data):
         for key in self.keys:
             if key in data:
-                data[key] = transforms.RandomAffine(0, shear=10, scale=(0.8,1.2))(data[key])
+                data[key] = RandomAffine(0, shear=10,
+                                         scale=(0.8, 1.2))(data[key])
             else:
-                raise KeyError(f'{key} is not a key of {data}')   
+                raise KeyError(f'{key} is not a key of {data}')
 
         return data
-    
-    
+
+
 class CenterCropd():
     def __init__(self, keys):
         self.keys = keys
@@ -113,12 +127,12 @@ class CenterCropd():
     def __call__(self, data):
         for key in self.keys:
             if key in data:
-                data[key] = transforms.CenterCrop(200)(data[key])
+                data[key] = CenterCrop(200)(data[key])
             else:
-                raise KeyError(f'{key} is not a key of {data}')   
+                raise KeyError(f'{key} is not a key of {data}')
 
         return data
-    
+
 
 class RandomCropd():
     def __init__(self, keys):
@@ -127,9 +141,9 @@ class RandomCropd():
     def __call__(self, data):
         for key in self.keys:
             if key in data:
-                data[key] = transforms.RandomCrop((375,375))(data[key])
+                data[key] = RandomCrop((375, 375))(data[key])
             else:
-                raise KeyError(f'{key} is not a key of {data}')       
+                raise KeyError(f'{key} is not a key of {data}')
 
         return data
 
@@ -141,13 +155,14 @@ class Normalized():
     def __call__(self, data):
         for key in self.keys:
             if key in data:
-                data[key] = transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))(data[key])
+                data[key] = Normalize((0.485, 0.456, 0.406),
+                                      (0.229, 0.224, 0.225))(data[key])
             else:
                 raise KeyError(f'{key} is not a key of {data}')
-                
-        return data    
 
-    
+        return data
+
+
 class Scale01d():
     def __init__(self, keys):
         self.keys = keys
@@ -157,111 +172,112 @@ class Scale01d():
             if key in data:
                 data[key] = data[key].float()/255
             else:
-                raise KeyError(f'{key} is not a key of {data}') 
+                raise KeyError(f'{key} is not a key of {data}')
 
-        return data  
+        return data
 
-    
+
 class RandomGaussianNoise():
     def __init__(self, sig=0.01, p=0.5):
         self.sig = sig
         self.p = p
-        
+
     def __call__(self, img):
         if random.random() > self.p:
             img += self.sig * torch.randn(img.shape)
 
         return img
 
-    
+
 class RandGaussNoised(RandomGaussianNoise):
     def __init__(self, keys):
         super().__init__()
         self.keys = keys
-        
+
     def __call__(self, data):
         for key in self.keys:
             if key in data:
-                 data[key] = RandomGaussianNoise()(data[key])
-            else:
-                raise KeyError(f'{key} is not a key of {data}')  
-
-        return data
-
-        
-class GridMask():
-    def __init__(self, dmin=90, dmax=160, ratio=0.8, p=0.6):
-        """Original Setting : dmin=90, dmax=300, ratio=0.7, p=0.5
-        after augmentation, again masking with (90, 160, 0.8, 0.6) 
-        [ dmin, dmax ] : range of the d in uniform random 
-        random variable probibilaty > p, swith on the function"""
-        self.dmin   = dmin 
-        self.dmax   = dmax
-        self.ratio  = ratio
-        self.p = p
-        
-    def __call__(self, Img):
-        if random.random() < self.p:
-            return Img
-        d = random.randint(self.dmin, self.dmax)
-        dx, dy = random.randint(0,d-1), random.randint(0,d-1)
-        sl = int((1-self.ratio)*d)
-        for i in range(dx, Img.shape[1], d):
-            for j in range(dy, Img.shape[2], d):
-                row_end, col_end = min(i+sl, Img.shape[1]), min(j+sl, Img.shape[2])
-                Img[:, i:row_end, j:col_end] = 0
-
-        return Img
-        
-        
-class GridMaskd(GridMask):
-    def __init__(self, keys):
-        super().__init__()
-        self.keys = keys
-        
-    def __call__(self, data):
-        for key in self.keys:
-            if key in data:
-                 data[key] = GridMask()(data[key])
+                data[key] = RandomGaussianNoise()(data[key])
             else:
                 raise KeyError(f'{key} is not a key of {data}')
 
         return data
 
- 
-        
+
+class GridMask():
+    def __init__(self, dmin=90, dmax=160, ratio=0.8, p=0.6):
+        """Original Setting : dmin=90, dmax=300, ratio=0.7, p=0.5
+        after augmentation, again masking with (90, 160, 0.8, 0.6)
+        [ dmin, dmax ] : range of the d in uniform random
+        random variable probibilaty > p, swith on the function"""
+        self.dmin = dmin
+        self.dmax = dmax
+        self.ratio = ratio
+        self.p = p
+
+    def __call__(self, Img):
+        if random.random() < self.p:
+            return Img
+        d = random.randint(self.dmin, self.dmax)
+        dx, dy = random.randint(0, d-1), random.randint(0, d-1)
+        sl = int((1-self.ratio)*d)
+        for i in range(dx, Img.shape[1], d):
+            for j in range(dy, Img.shape[2], d):
+                row_end = min(i+sl, Img.shape[1])
+                col_end = min(j+sl, Img.shape[2])
+                Img[:, i:row_end, j:col_end] = 0
+
+        return Img
+
+
+class GridMaskd(GridMask):
+    def __init__(self, keys):
+        super().__init__()
+        self.keys = keys
+
+    def __call__(self, data):
+        for key in self.keys:
+            if key in data:
+                data[key] = GridMask()(data[key])
+            else:
+                raise KeyError(f'{key} is not a key of {data}')
+
+    return data
+
+
 class Transforms():
-    def __init__(self, size = 500):
+    def __init__(self, size=500):
         self.size = size
-        
+
     def train_transforms(self):
-        train_trans = transforms.Compose([read_imaged(keys=['image']),
-                                          Scale01d(keys=['image']),
-                                          Resized(keys=['image'], size=(self.size,self.size)),
-                                          RandGaussNoised(keys=['image']),
-                                          transforms.RandomOrder(
-                                              [RandHoriFlipd(keys=['image']),
-                                              RandRotd(keys=['image'], degrees=10),
-                                              RandAffined(keys=['image']) 
-                                              ]),
-                                          GridMaskd(keys=['image']),
-                                          Normalized(keys=['image'])
-                                         ])
+        train_trans = Compose([read_imaged(keys=['image']),
+                               Scale01d(keys=['image']),
+                               Resized(keys=['image'],
+                                       size=(self.size, self.size)),
+                               RandGaussNoised(keys=['image']),
+                               RandomOrder(
+                                   [RandHoriFlipd(keys=['image']),
+                                    RandRotd(keys=['image'], degrees=10),
+                                    RandAffined(keys=['image'])]),
+                               GridMaskd(keys=['image']),
+                               Normalized(keys=['image'])])
 
         return train_trans
 
     def valid_transforms(self):
-        valid_trans = transforms.Compose([read_imaged(keys=['image']),
-                                          Scale01d(keys=['image']),
-                                          Resized(keys=['image'], size=(self.size,self.size)),
-                                          Normalized(keys=['image'])])
+        valid_trans = Compose([read_imaged(keys=['image']),
+                               Scale01d(keys=['image']),
+                               Resized(keys=['image'],
+                                       size=(self.size, self.size)),
+                               Normalized(keys=['image'])])
 
         return valid_trans
 
     def test_transforms(self):
-        test_trans = transforms.Compose([read_imaged(keys=['image']),
-                                        Scale01d(keys=['image']),
-                                        Resized(keys=['image'], size=(self.size,self.size)),
-                                        Normalized(keys=['image'])])
+        test_trans = Compose([read_imaged(keys=['image']),
+                              Scale01d(keys=['image']),
+                              Resized(keys=['image'],
+                                      size=(self.size, self.size)),
+                              Normalized(keys=['image'])])
 
         return test_trans
